fb46f3e13460528a58e01f9bb28eba0e9a608d8e
Yang Wu <wuy66999@gmail.com>
2024-10-30 14:34:27 -0400

    finish food section

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Homescreen/>
       
     </div>
   );
---
+++ client/src/pizzadata.js
@@ -0,0 +1,86 @@
+const pizzas = [{
+  name: "PEPPER BARBECUE CHICKEN",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 200,
+      medium: 350,
+      large: 400
+    }
+  ],
+  category: "nonveg",
+  image: "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+  description: "Pepper Barbecue Chicken I Cheese",
+},
+{
+  name: "Non Veg Supreme Gold",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 210,
+      medium: 360,
+      large: 400
+    }
+  ],
+  category: "nonveg",
+  image: "https://www.dominos.co.in/files/items/Non-Veg_Supreme.jpg",
+  description: "Non Veg Supreme Gold I Cheese",
+},
+{
+  name: "Deluxe Veggie",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 220,
+      medium: 320,
+      large: 410
+    }
+  ],
+  category: "veg",
+  image: "https://www.dominos.co.in/files/items/Deluxe_Veggie.jpg",
+  description: "Deluxe Veggie with multiple veg",
+},
+{
+  name: "Veg Extravaganza",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 190,
+      medium: 270,
+      large: 320
+    }
+  ],
+  category: "veg",
+  image: "https://www.dominos.co.in/files/items/Veg_Extravaganza.jpg",
+  description: "Jalapeno & Red Paprika Pizza"
+},
+{
+  name: "Chicken Golden Delight",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 200,
+      medium: 350,
+      large: 400
+    }
+  ],
+  category: "nonveg",
+  image: "https://www.dominos.co.in/files/items/Chicken_Golden_Delight.jpg",
+  description: "Chicken Golden Delight with Delicious Chicken"
+},
+{
+  name: "Paneer Makhani",
+  size: ["small", "medium", "large"],
+  prices: [
+    {
+      small: 260,
+      medium: 390,
+      large: 450
+    }
+  ],
+  category: "nonveg",
+  image: "https://www.dominos.co.in/files/items/Mexican_Green_Wave.jpg",
+  description: "Paneer Makhani with cheese"
+},
+];
+export default pizzas;

46d511a845f0bbfa4c9ce9075f81060a79d237e4
Yang Wu <wuy66999@gmail.com>
2024-11-01 18:14:41 -0400

    Backend connect with MongoDB successfully

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+var mongoURL = 'mongodb+srv://wuy:Shanxiawoyan0809!@cluster0.yw2fy.mongodb.net/FoodOrderingWeb'
+
+mongoose.connect(mongoURL, {useUnifiedTopology:true, useNewUrlParser:true})
+var db = mongoose.connection
+db.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+db.on('error' , () =>{
+    console.log(`Mongo DB Connection failed`);
+})
+module.exports = mongoose

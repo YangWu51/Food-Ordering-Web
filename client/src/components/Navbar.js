473486f7953739db6eab67f6c0bd955f2f196f85
Yang Wu <wuy66999@gmail.com>
2024-11-09 14:04:14 -0600

    finish my cart page add & delete & total prices

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useSelector, useDispatch } from 'react-redux';
+
 export default function Navbar() {
     const cartstate = useSelector(state=>state.cartReducer)
     return (
         <div>
             <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-body rounded">
-                <a className="navbar-brand" href="#" >YANG PIZZA</a>
+                <a className="navbar-brand" href="/" >YANG PIZZA</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- client/src/screens/Cartscreen.js
+++ client/src/screens/Cartscreen.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux"; 
+import { addToCart } from "../actions/cartActions";
+import { deleteFromCart } from "../actions/cartActions";
 export default function Cartscreen(){
     const cartstate = useSelector(state=>state.cartReducer)
     const cartItems = cartstate.cartItems
+    var subtotal = cartItems.reduce((x, item)=> x+item.price , 0)
+    const dispatch = useDispatch()
     return (
         <div>
             <div className="row justify-content-center">
@@ -15,9 +19,9 @@ export default function Cartscreen(){
                             <h1>{item.name} [{item.size}]</h1>
                             <h1>Price : {item.quantity} * {item.prices[0][item.size]} = {item.price}</h1>
                             <h1 style={{display: 'inline'}}>Quantity : </h1>
-                            <i className="fa-solid fa-plus"></i>
+                            <i className="fa-solid fa-plus" onClick={()=>{dispatch(addToCart(item, Number(item.quantity)+1, item.size ))}}></i>
                             <b>{item.quantity}</b>
-                            <i className="fa-solid fa-minus"></i>
+                            <i className="fa-solid fa-minus" onClick={()=>{dispatch(addToCart(item, Number(item.quantity)-1, item.size))}}></i>
                             <hr/>
                         </div>
 
@@ -26,14 +30,16 @@ export default function Cartscreen(){
                         </div>
 
                         <div className="m-1 w-100">
-                        <i className="fa-solid fa-trash mt-5"></i>
+                        <i className="fa-solid fa-trash mt-5" onClick={()=>{dispatch(deleteFromCart(item))}}></i>
                         </div>
                                  </div>
                     })}
 
-                    
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-4 text-end">
+                    <h2 style={{fontSize:'45px'}}>subtotal : {subtotal} /-</h2>
+                    <button className="btn">CHECK OUT</button>
+
                 </div>
 
 

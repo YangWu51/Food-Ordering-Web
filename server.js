d9b654f02096e956529fd34090bad8f6a843b472
Yang Wu <wuy66999@gmail.com>
2024-11-29 21:19:13 -0600

    feat: port

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const express = require("express");
-const Pizza = require('./models/pizzaModel')
-const cors = require('cors')
+const Pizza = require('./models/pizzaModel');
+const cors = require('cors');
 const app = express();
-const db = require("./db.js")
+const db = require("./db.js");
+
 app.use(express.json());
 app.use(cors());
-const pizzasRoute = require('./routes/pizzasRoute')
-const userRoute = require('./routes/userRoute')
-const ordersRoute = require('./routes/ordersRoute')
-app.use('/api/pizzas/', pizzasRoute)
-app.use('/api/users/', userRoute)
-app.use('/api/orders/' , ordersRoute)
+
+// Routes
+const pizzasRoute = require('./routes/pizzasRoute');
+const userRoute = require('./routes/userRoute');
+const ordersRoute = require('./routes/ordersRoute');
+
+app.use('/api/pizzas/', pizzasRoute);
+app.use('/api/users/', userRoute);
+app.use('/api/orders/', ordersRoute);
+
+// Root route
 app.get("/", (req, res) => {
-    res.send("Server working " + port);
+    res.send(`Server working on port ${port}`);
 });
 
-const port = process.env.PORT || 8000;
+// Define port
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => `Server running on port port`);
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});

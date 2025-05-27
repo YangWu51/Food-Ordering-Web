4bfe095950c4277a0c6c60bb30d56d56a05312f0
Yang Wu <wuy66999@gmail.com>
2025-05-27 17:29:15 -0500

    update db.js

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-var mongoURL = 'mongodb+srv://wuy:Shanxiawoyan0809!@cluster0.yw2fy.mongodb.net/FoodOrderingWeb'
+const mongoURL = process.env.MONGO_URL;
 
-mongoose.connect(mongoURL, 
-    // {useUnifiedTopology:true, useNewUrlParser:true}
-)
-var db = mongoose.connection
-db.on('connected' , ()=>{
-    console.log(`Mongo DB Connection Successfull`);
-})
-db.on('error' , () =>{
-    console.log(`Mongo DB Connection failed`);
-})
-module.exports = mongoose
+mongoose.connect(mongoURL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('connected', () => {
+    console.log("Mongo DB Connection Successful");
+});
+db.on('error', () => {
+    console.log("Mongo DB Connection Failed");
+});
+
+module.exports = mongoose;

0f44ae45bf3eecd933d1b9e33d47d2c7836f445f
Yang Wu <wuy66999@gmail.com>
2025-05-27 17:39:13 -0500

    ..

--- db.js
+++ db.js
@@ -1,18 +1,32 @@
-const mongoose = require("mongoose");
 
+const mongoose = require("mongoose");
 const mongoURL = process.env.MONGO_URL;
 
-mongoose.connect(mongoURL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-});
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
-db.on('connected', () => {
-    console.log("Mongo DB Connection Successful");
-});
-db.on('error', () => {
-    console.log("Mongo DB Connection Failed");
-});
+db.on('connected', () => console.log(`MongoDB Connected`));
+db.on('error', () => console.log(`MongoDB Connection Failed`));
 
 module.exports = mongoose;
+
+
+
+// const mongoose = require("mongoose");
+
+// const mongoURL = process.env.MONGO_URL;
+
+// mongoose.connect(mongoURL, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// });
+
+// const db = mongoose.connection;
+// db.on('connected', () => {
+//     console.log("Mongo DB Connection Successful");
+// });
+// db.on('error', () => {
+//     console.log("Mongo DB Connection Failed");
+// });
+
+// module.exports = mongoose;
